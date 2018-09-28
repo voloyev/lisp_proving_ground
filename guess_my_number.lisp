2e88b04a9678394be02071804a0963ce5b51fe66
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-09-28 14:27:05 +0300

    Add functions to calculate

--- guess_my_number.lisp
+++ guess_my_number.lisp
@@ -3,3 +3,16 @@
 
 (defun guess-my-number ()
   (ash (+ *small* *big*) -1))
+
+(defun smaller()
+  (setf *small* (1- (guess-my-number)))
+  (guess-my-number))
+
+(defun bigger()
+  (setf *big* (1+ (guess-my-number)))
+  (guess-my-number))
+
+(defun start-over()
+  (defparameter *small* 1)
+  (defparameter *big* 100)
+  (guess-my-number))
